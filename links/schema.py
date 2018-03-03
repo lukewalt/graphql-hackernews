a3a869a5d0e02798b6d2f00bdfc6aa71ef6b2ffd
Luke <luke.woodward@parallontechnology.com>
2018-03-03 14:16:29 -0600

    created mutation to send data to server

--- links/schema.py
+++ links/schema.py
@@ -17,3 +17,29 @@ class Query(graphene.ObjectType):
         return Link.objects.all()
 
 
+#defines a mutation class
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    #defines data that is sent to the server
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # mutation method: creates link on the database using the defined data items
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+
+
+#creates mutation class with a field to be resolved which points to the mutation defined above
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
