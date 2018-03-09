09fe25003fcdc7c6d4a8f5228faee8185adff80f
Luke <luke.woodward@parallontechnology.com>
2018-03-09 14:04:57 -0600

    fixed links reference in main schema

--- links/schema_relay.py
+++ links/schema_relay.py
@@ -28,3 +28,27 @@ class VoteNode(DjangoObjectType):
 class RelayQuery(graphene.ObjectType):
     relay_link = graphene.relay.Node.Field(LinkNode)
     relay_links = DjangoFilterConnectionField(LinkNode, filterset_class=LinkFilter)
+
+class RelayCreateLink(graphene.relay.ClientIDMutation):
+    link = graphene.Field(LinkNode)
+
+    class Input:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate_and_get_payload(root, info, **input):
+        user = info.context.user or None
+
+        link = Link(
+            url=input.get('url'),
+            description=input.get('description'),
+            posted_by=user,
+        )
+        link.save()
+
+        return RelayCreateLink(link=link)
+
+class RelayMutation(graphene.AbstractType):
+    relay_create_link = RelayCreateLink.Field()
+
+    
